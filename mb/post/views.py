49d9ffa00ae9b220b671195a87295b3af745caf7
Mujadid Ali <mujadidali23@gmail.com>
2019-01-14 01:32:07 +0500

    First Commit Chapter4

---
+++ mb/post/views.py
@@ -0,0 +1,11 @@
+
+from django.views.generic import ListView
+from .models import Post
+
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+# Create your views here.
